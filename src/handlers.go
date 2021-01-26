037e2f422bd821a032e16f8689878b4f4171dd84
jmaster98 <jo-ash@procentrica.co.za>
2021-01-26 16:20:24 +0200

    allows for handling of multiple funclocs

--- src/handlers.go
+++ src/handlers.go
@@ -14,65 +14,81 @@ func (s *Server) handlePostToAssetRegister() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//get JSON payload.
 		body, err := ioutil.ReadAll(r.Body)
+		funclocList := FunclocList{}
+		err = json.Unmarshal(body, &funclocList)
 
-		//post to crud service
-		req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/toAssetRegister", "application/json", bytes.NewBuffer(body))
+		//response variable for posted assets
+		assetresponse := ResponseList{}
+		assetresponse.ResponseList = []ARPostResult{}
 
-		//check for response error of 500
-		if respErr != nil {
-			w.WriteHeader(500)
-			fmt.Fprint(w, respErr.Error())
-			fmt.Println("Error in communication with CRUD service endpoint for request to post assets")
-			return
-		}
-		if req.StatusCode != 200 {
-			fmt.Fprint(w, "Request to DB can't be completed...")
-			fmt.Println("unable to process post assets")
-		}
-		if req.StatusCode == 500 {
-			w.WriteHeader(500)
-
-			bodyBytes, err := ioutil.ReadAll(req.Body)
-			if err != nil {
-				log.Fatal(err)
+		//For loop which deals with
+		for _, element := range funclocList.Flist {
+			//	for i := 0; i < len(funclocList.Flist); i++ {
+			//fmt.Println("JSON:", funclocList.Flist[i])
+			js, jserr1 := json.Marshal(element)
+			if jserr1 != nil {
+				w.WriteHeader(500)
+				fmt.Fprintf(w, "Unable to create JSON result object from DB result to post Funcloc flex value.")
+				return
 			}
-			bodyString := string(bodyBytes)
-			fmt.Fprintf(w, "Request to DB can't be completed...  "+bodyString)
-			fmt.Println("Request to DB can't be completed...  " + bodyString)
-			return
+			//post to crud service
+			req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/toAssetRegister", "application/json", bytes.NewBuffer(js))
+			//check for response error of 500
+			if respErr != nil {
+				w.WriteHeader(500)
+				fmt.Fprint(w, respErr.Error())
+				fmt.Println("Error in communication with CRUD service endpoint for request to post assets")
+				return
+			}
+			if req.StatusCode != 200 {
+				fmt.Fprint(w, "Request to DB can't be completed...")
+				fmt.Println("unable to process post assets")
+			}
+			if req.StatusCode == 500 {
+				w.WriteHeader(500)
+				bodyBytes, err := ioutil.ReadAll(req.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				bodyString := string(bodyBytes)
+				fmt.Fprintf(w, "Request to DB can't be completed...  "+bodyString)
+				fmt.Println("Request to DB can't be completed...  " + bodyString)
+				return
+			}
+			if err != nil {
+				w.WriteHeader(500)
+				fmt.Fprint(w, err.Error())
+				fmt.Println("Posting is not able to be completed by internal error")
+				return
+			}
+			//close the request
+			defer req.Body.Close()
+			//create new response struct
+			var postAssetsResponse ARPostResult
+
+			//decode request into decoder which converts to the struct
+			decoder := json.NewDecoder(req.Body)
+			err = decoder.Decode(&postAssetsResponse)
+			if err != nil {
+				w.WriteHeader(500)
+				fmt.Fprint(w, err.Error())
+				fmt.Println("Error occured in decoding Posting response")
+				return
+			}
+
+			assetresponse.ResponseList = append(assetresponse.ResponseList, ARPostResult{postAssetsResponse.FunclocSuccess, postAssetsResponse.FunclocMessage, postAssetsResponse.FunclocID, postAssetsResponse.FunclocflexvalSuccess, postAssetsResponse.FunclocflexvalMessage, postAssetsResponse.FunclocnodeSuccess, postAssetsResponse.FunclocnodeMessage, postAssetsResponse.FunclocnodeID, postAssetsResponse.FuncloclinkSuccess, postAssetsResponse.FuncloclinkMessage, postAssetsResponse.FunclocnodeflexvalSuccess, postAssetsResponse.FunclocnodeflexvalMessage, postAssetsResponse.AssetSuccess, postAssetsResponse.AssetMessage, postAssetsResponse.PostedAssetList, postAssetsResponse.AssetflexvalSuccess, postAssetsResponse.AssetflexvalMessage, postAssetsResponse.ObservationflexvalSuccess, postAssetsResponse.ObservationflexvalMessage})
+
 		}
-		if err != nil {
+
+		//convert struct back to JSON
+		js, jserr4 := json.Marshal(assetresponse)
+
+		if jserr4 != nil {
 			w.WriteHeader(500)
-			fmt.Fprint(w, err.Error())
-			fmt.Println("Posting is not able to be completed by internal error")
+			fmt.Fprintf(w, "Unable to create JSON result object from DB result to update Asset.")
 			return
 		}
 
-		//close the request
-		defer req.Body.Close()
-
-		//create new response struct
-		var postAssetsResponse ARPostResult
-
-		//decode request into decoder which converts to the struct
-		decoder := json.NewDecoder(req.Body)
-
-		err = decoder.Decode(&postAssetsResponse)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprint(w, err.Error())
-			fmt.Println("Error occured in decoding Posting response")
-			return
-		}
-		js, jserr := json.Marshal(postAssetsResponse)
-		if jserr != nil {
-			w.WriteHeader(500)
-			fmt.Fprint(w, jserr.Error())
-			fmt.Println("Error occured when trying to marshal the response to register user")
-			return
-		}
-
-		//return back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
--- src/types.go
+++ src/types.go
@@ -23,6 +23,113 @@ type Asset struct {
 	ID   string `json:"id,omitempty"`
 }
 
+type AssetFlexVal struct {
+	ID      string `json:"id,omitempty"`
+	AssetID string `json:"assetid,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Value   string `json:"value,omitempty"`
+}
+
+type ObservationFlexVal struct {
+	ID      string `json:"id,omitempty"`
+	AssetID string `json:"assetid,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Value   string `json:"value,omitempty"`
+}
+
+type toAssetRegister struct {
+	ID                          string               `json:"id"`
+	Name                        string               `json:"name"`
+	AssetType                   string               `json:"assettype"`
+	CompatibleUnitID            string               `json:"compatibleunitid"`
+	DerecognitionDate           string               `json:"derecognitiondate,omitempty"`
+	DerecognitionValue          string               `json:"derecognitionvalue,omitempty"`
+	Description                 string               `json:"description,omitempty"`
+	Dimension1Value             string               `json:"dimension1value,omitempty"`
+	Dimension2Value             string               `json:"dimension2value,omitempty"`
+	Dimension3Value             string               `json:"dimension3value,omitempty"`
+	Dimension4Value             string               `json:"dimension4value,omitempty"`
+	Dimension5Value             string               `json:"dimension5value,omitempty"`
+	Extent                      string               `json:"extent,omitempty"`
+	ExtentConfidence            string               `json:"extentconfidence,omitempty"`
+	ManufactureDate             string               `json:"manufacturedate,omitempty"`
+	ManufactureDateConfidence   string               `json:"manufacturedateconfidence,omitempty"`
+	TakeOnDate                  string               `json:"takeondate,omitempty"`
+	SerialNo                    string               `json:"serialno,omitempty"`
+	Latitude                    string               `json:"lat"`
+	Longitude                   string               `json:"lon"`
+	Geom                        string               `json:"geom"`
+	FunclocID                   string               `json:"funclocid"`
+	InstallDate                 string               `json:"installdate"`
+	Status                      string               `json:"status,omitempty"`
+	Age                         string               `json:"age,omitempty"`
+	CarryingValueClosingBalance string               `json:"carryingvalueclosingbalance,omitempty"`
+	CarryingValueOpeningBalance string               `json:"carryingvalueopeningbalance,omitempty"`
+	CostClosingBalance          string               `json:"costclosingbalance,omitempty"`
+	CostOpeningBalance          string               `json:"costopeningbalance,omitempty"`
+	Crc                         string               `json:"crc,omitempty"`
+	DepreciationClosingBalance  string               `json:"depreciationclosingbalance,omitempty"`
+	DepreciationOpeningBalance  string               `json:"depreciationopeningbalance,omitempty"`
+	ImpairmentClosingBalance    string               `json:"impairmentclosingbalance,omitempty"`
+	ImpairmentOpeningBalance    string               `json:"impairmentopeningbalance,omitempty"`
+	ResidualValue               string               `json:"residualvalue,omitempty"`
+	RulYears                    string               `json:"rulyears,omitempty"`
+	Drc                         string               `json:"drc,omitempty"`
+	Fy                          string               `json:"fy,omitempty"`
+	AssetValID                  string               `json:"assetvalid"`
+	FlvList                     []AssetFlexVal       `json:"assetflexvals"`
+	OFlvList                    []ObservationFlexVal `json:"observationflexvals"`
+}
+
+type toAssetRegsiterList struct {
+	Alist []toAssetRegister `json:"assets"`
+}
+
+type FunclocFlexVal struct {
+	ID        string `json:"id,omitempty"`
+	FunclocID string `json:"funclocid,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Value     string `json:"value,omitempty"`
+}
+
+type Funcloc struct {
+	FunclocID   string            `json:"funclocid,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	Description string            `json:"description,omitempty"`
+	Latitude    string            `json:"latitude,omitempty"`
+	Longitude   string            `json:"longitude,omitempty"`
+	Geom        string            `json:"geom,omitempty"`
+	FLNlist     []FunclocNode     `json:"funclocnodes,omitempty"`
+	FLFVlist    []FunclocFlexVal  `json:"funclocflexvals,omitempty"`
+	Alist       []toAssetRegister `json:"assets,omitempty"`
+}
+
+type FunclocList struct {
+	Flist []Funcloc `json:"funcloc"`
+}
+
+type FunclocNodeFlexVal struct {
+	ID            string `json:"id,omitempty"`
+	FunclocNodeID string `json:"funclocnodeid,omitempty"`
+	Name          string `json:"name,omitempty"`
+	Value         string `json:"value,omitempty"`
+}
+type FunclocNode struct {
+	ID         string               `json:"funclocnodeid,omitempty"`
+	Name       string               `json:"name,omitempty"`
+	AliasName  string               `json:"aliasname,omitempty"`
+	Latitude   string               `json:"latitude,omitempty"`
+	Longitude  string               `json:"longitude,omitempty"`
+	Geom       string               `json:"geom,omitempty"`
+	NodeTypeID string               `json:"nodetypeid,omitempty"`
+	ParentID   string               `json:"parentid,omitempty"`
+	FLNFVlist  []FunclocNodeFlexVal `json:"funclocnodeflexvals,omitempty"`
+}
+
+type FunclocNodeList struct {
+	Fnodelist []FunclocNode `json:"funclocnode"`
+}
+
 type ARPostResult struct {
 	FunclocSuccess            bool    `json:"funclocsuccess,omitempty"`
 	FunclocMessage            string  `json:"funclocmessage,omitempty"`
@@ -43,4 +150,8 @@ type ARPostResult struct {
 	AssetflexvalMessage       string  `json:"assetflexvalmessage,omitempty"`
 	ObservationflexvalSuccess bool    `json:"observationflexvalsuccess,omitempty"`
 	ObservationflexvalMessage string  `json:"observationflexvalmessage,omitempty"`
+}
+
+type ResponseList struct {
+	ResponseList []ARPostResult `json:"result"`
 }
